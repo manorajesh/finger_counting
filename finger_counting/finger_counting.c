ff7ab59e26a6adc9cf7f5a6df7c0bc2f3c74cf20
Mano Rajesh <mano@raje.sh>
2021-10-22 17:29:42 -0700

    Added if statement to determine, on given criteria, whether to print or not

--- finger_counting/finger_counting.c
+++ finger_counting/finger_counting.c
@@ -5,17 +5,17 @@ void main()
 	int finger = 0;											//variable for finger (i.e. 1 is thumb, 2 is pointer, etc.)
 	int number = 0;											//varible for number
 
-	while (number < 2000) {									//the loop will end when number is at 2000
+	while (number <= 2000) {								//the loop will end when number is at 2000
 		if (finger < 5) {									//only counting on one "hand" or 5 fingers
 			++number;										//increment the number variable as it counts up on the finger
 			++finger;										//increment the finger to next one
-			printf("finger %d: %d\n", finger, number);		//print here since two loops
+			if (number % 20 == 0) { printf("finger %d: %d\n", finger, number); }		//print here since two loops
 		}
 		else if (finger >= 5) {								//if finger counter is at pinky, go back to ring
 			while (finger != 1) {							//once finger is back at thumb, go back to pointer
 				--finger;									//going backwards in finger count
 				++number;									//increasing number
-				printf("finger %d: %d\n", finger, number);	//print here since two loops
+				if (number % 20 == 0) { printf("finger %d: %d\n", finger, number); }	//print here since two loops
 			}
 		}
 	}
